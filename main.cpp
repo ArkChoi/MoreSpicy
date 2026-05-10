76c7f42778d92af75d6d9ae96d60be1245f86817
ArkChoi <ark.goya@gmail.com>
2026-05-11 01:08:48 +0900

    SetUp

---
+++ main.cpp
@@ -0,0 +1,59 @@
+//프로그래머스 : 더 맵게 (Lv. 2)
+//힙을 사용하는 문제 (힙 자체 제작 버전)
+//유사 문제 : 파일 병합 문제
+#include <iostream>
+#include <string>
+#include <vector>
+
+void VectorInput(std::vector<int>* InScoville);
+int MaxScovilleInput();
+int solution(std::vector<int> scoville, int K);
+
+int main()
+{
+    std::vector<int> Scoville;
+    int MaxScoville;
+    int TempResult;
+
+    VectorInput(&Scoville);
+    MaxScoville = MaxScovilleInput();
+
+    TempResult = solution(Scoville, MaxScoville);
+    //std::cout << TempResult;
+
+    for (int i = 0; i < Scoville.size(); i++)
+    {
+        std::cout << Scoville[i] << " | ";
+    }
+    std::cout << std::endl << MaxScoville;
+
+    return 0;
+}
+
+void VectorInput(std::vector<int>* InScoville)
+{
+    std::cout << "VectorInputSize : ";
+    int TempLoop = 0;
+    std::cin >> TempLoop;
+    for (int i = 0; i < TempLoop; i++)
+    {
+        std::cout << "VectorInputNum : ";
+        int TempNumber = 0;
+        std::cin >> TempNumber;
+        InScoville->push_back(TempNumber);
+    }
+}
+
+int MaxScovilleInput()
+{
+    std::cout << "MaxScovilleInput : ";
+    int TempBudget = 0;
+    std::cin >> TempBudget;
+    return TempBudget;
+}
+
+int solution(std::vector<int> scoville, int K)
+{
+    int answer = 0;
+    return answer;
+}
